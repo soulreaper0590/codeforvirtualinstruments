9dee2680bbd5e3c8bcd68eacaf5291b3344a4974
shashwathsb1998 <shashwathsb1998@gmail.com>
2017-09-18 22:34:50 +0530

    Update pySerial.py

--- pySerial.py
+++ pySerial.py
@@ -2,21 +2,21 @@ import time
 import serial
 
 
-ser=Serial.serial('COM13',9600)
-int c2=0
-float a[3]
+ser=Serial.serial('COM9',9600)
+c2=0
+a[3]
 while 1:
     magvalues=ser.read()
-    int c=0;
-    for(int j=0;j<3;j++)
-        for(int i=c+1;i<len(magvalues);i++):
-            if(magvalues[i]=' ')
+    c=0;
+    for j in range(0,3):
+        for i in range (c+1,len(magvalues)):
+            if magvalues[i]==' ':
                 c2=i
                 break
         a[j]=float(magvalues[c,c2])
         c=c2
-float Xm, Ym, Zm, heading, declination, headingDegrees, headingFiltered   
-float PI = 3.1415 
+Xm, Ym, Zm, heading, declination, headingDegrees, headingFiltered=0.0 
+PI = 3.1415 
 Xm = 2 * a[0] * 0.00092
 Ym = 2 * a[1] * 0.00092
 Zm = 2 * a[2] * 0.00092
@@ -26,19 +26,12 @@ heading = arctan2(Ym, Xm)
 declination = 0.021 
 heading += declination
   
-if(heading <0) 
+if heading <0:
 	heading += 2*PI
 
-if(heading > 2*PI)
+if heading > 2*PI:
 	heading -= 2*PI
 
 headingDegrees = heading * 180/PI 
  
 headingFiltered = headingFiltered*0.85 + headingDegrees*0.15
-    
-        
-
-
-            
-        
-    

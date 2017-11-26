7437e06ffe5e821f7a33a5e9957c25a63ddb78bd
RishbhaJain <rishbhajain8@gmail.com>
2017-11-27 00:23:52 +0530

    Update guitar1.ino

--- guitar1.ino
+++ guitar1.ino
@@ -1,4 +1,7 @@
-  int fingerStatus(float finger);
+  int fingerStatus1(float finger);
+  int fingerStatus2(float finger);
+  int fingerStatus3(float finger);
+  int fingerStatus4(float finger);
   int region(float stretch);
  
 void setup(){
@@ -15,10 +18,10 @@ void setup(){
   float finger2 = analogRead(2);
   float finger3 = analogRead(3);
   float finger4 = analogRead(4);
-  finger11 = fingerStatus(finger1);
-    int finger22 = fingerStatus(finger2);
-      int finger33 = fingerStatus(finger3);
-        int finger44 = fingerStatus(finger4);
+  finger11 = fingerStatus1(finger1);
+    int finger22 = fingerStatus2(finger2);
+      int finger33 = fingerStatus3(finger3);
+        int finger44 = fingerStatus4(finger4);
   Serial.print(finger1);
   
   Serial.print(" "); Serial.print(finger2);
@@ -26,23 +29,23 @@ void setup(){
    Serial.print(" "); Serial.println(finger4);
 
   if(finger11==2&&finger22==1&&finger33==1&&finger44==2){
-    Serial.println("Gmajor");
+    Serial.println("G");
   }
   
   if(finger11==2&&finger22==2&&finger33==1){
-    Serial.println("Cmajor");
+    Serial.println("C");
   }
   if(finger11==2&&finger22==2&&finger33==2&&finger44==0){
-    Serial.println("Dmajor");
+    Serial.println("D");
   }
   if(finger11==2&&finger22==2&&finger33==1&&finger44==1){
-    Serial.println("Fmajor");
+    Serial.println("F");
   }
   if(finger11==2&&finger22==1&&finger33==1&&finger44==1){
-    Serial.println("Emajor");
+    Serial.println("E");
   }
   if(finger11==2&&finger22==2&&finger33==2&&finger44==1){
-    Serial.println("Amajor");
+    Serial.println("A");
   }
   
   
@@ -53,19 +56,68 @@ void setup(){
   
   
   }
-  int fingerStatus(float finger){
+  int fingerStatus1(float finger){
     int fingerReturn= 0;
      
-    if(finger<=550&&finger>=565){
-      fingerReturn=1;
+    if(finger>=350){
+      fingerReturn=0;
       }
-      if(finger>575){
-        fingerReturn = 2;
+      if(finger<350&&finger>=250){
+        fingerReturn = 1;
         }
+       if(finger<250)
+       fingerReturn=2;
     
     
     return fingerReturn;
     }
+    int fingerStatus2(float finger){
+    int fingerReturn= 0;
+     
+    if(finger>=450){
+      fingerReturn=0;
+      }
+      if(finger<450&&finger>=370){
+        fingerReturn = 1;
+        }
+       if(finger<370)
+       fingerReturn=2;
+    
+    
+    return fingerReturn;
+    }
+int fingerStatus3(float finger){
+    int fingerReturn= 0;
+     
+    if(finger>=370){
+      fingerReturn=0;
+      }
+      if(finger<370&&finger>=337){
+        fingerReturn = 1;
+        }
+       if(finger<337)
+       fingerReturn=2;
+    
+    
+    return fingerReturn;
+    }
+int fingerStatus4(float finger){
+    int fingerReturn= 0;
+     
+    if(finger>=430){
+      fingerReturn=0;
+      }
+      if(finger<430&&finger>=340){
+        fingerReturn = 1;
+        }
+       if(finger<340)
+       fingerReturn=2;
+    
+    
+    return fingerReturn;
+    }
+
+    
 
   int region(float stretch)
   {
@@ -74,8 +126,7 @@ void setup(){
       regionReturn=1;
     else if(stretch>=630&&stretch<690)
       regionReturn=2;
-    else ()
+    else 
       regionReturn=3;
     return regionReturn;
   }
-

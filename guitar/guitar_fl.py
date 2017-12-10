13448c7e6b2e65d89cb7d5052676632c1ee90d29
shashwathsb1998 <shashwathsb1998@gmail.com>
2017-12-10 19:04:31 +0530

    Update guitar_fl.py

--- guitar/guitar_fl.py
+++ guitar/guitar_fl.py
@@ -2,23 +2,40 @@ import keyboard
 import time
 import serial
     
-
-Var1=serial.Serial('COM7',9600)
-time.sleep(1)
+#Var1=serial.Serial('COM22',9600)
+#import win32com.client as comclt
+Var2=serial.Serial('COM13',9600)
+#wsh= comclt.Dispatch("WScript.Shell")
+ 
 while 1:
-    data='z'
-    
-    data=Var1.read()
-    
-    if data == 'Z':
-	keyboard.press_release("z")
-    if data == 'X':
-	keyboard.press_release("x")
-    if data == 'C':
-	keyboard.press_release("c")
-     if data == 'V':
-	keyboard.press_release("v")
-        
+    #data1='z'
+    #data2='z'
+    #data1=Var1.read()
+    data2=Var2.readline().decode('ascii');
+    '''if data2 == 'a':
+        keyboard.press("z")
+        time.sleep(5)
+        keyboard.release("z")'''
+    if (data2[0] == 'Z'):
+        keyboard.write('z')
+        time.sleep(0.5)
+        keyboard.release('z')
+    if data2[0] == 'X':
+        keyboard.press("x")
+        time.sleep(0.5)
+        keyboard.release("x")
+    if data2[0] == 'C':
+        keyboard.press("c")
+        time.sleep(0.5)
+        keyboard.release("c")
+    if data2[0] == 'V':
+        keyboard.press("v")
+        time.sleep(0.5)
+        keyboard.release("v")
+    '''if data2 == 'f':
+        keyboard.press("z")
+        time.sleep(5)
+        keyboard.release("z")'''
 
         
    

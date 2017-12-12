74ba2385b7ae227905f03bb2fed7817463c9d925
Prakhar Thakuria <prakharthakuria@rocketmail.com>
2017-12-12 16:22:17 +0530

    Update B_slave_module.ino

--- BluetoothModule/B_slave_module.ino
+++ BluetoothModule/B_slave_module.ino
@@ -2,30 +2,30 @@
 SoftwareSerial BTserial(10, 11);
 
 char state;
-const int ledPin = 8;  //the pin your led is connected to
-const int buttonPin = 2;  //the pin your button is connected to
+//const int ledPin = 8;  //the pin your led is connected to
+//const int buttonPin = 2;  //the pin your button is connected to
 
-int buttonState = 0;
+//int buttonState = 0;
 
 void setup() {
 
   Serial.begin(9600);
   BTserial.begin(38400);
 
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+ // pinMode(ledPin, OUTPUT);
+  //pinMode(buttonPin, INPUT);
 
 }
 
 void loop() {
 
   if (BTserial.available() > 0) { // Checks whether data is comming from the serial port
-    Serial.print("hey");
+    //Serial.print("hey");
     state = BTserial.read();
     Serial.println(state);// Reads the data from the serial port
   }
   // Controlling the LED
-  Serial.println(state);
-  delay(2000);
+ // Serial.println(state);
+  delay(100);
   
 }

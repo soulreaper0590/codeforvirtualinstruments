b21a93ac586267dbd3644b5c87d6c94afd0b0ad6
Prakhar Thakuria <prakharthakuria@rocketmail.com>
2017-12-13 16:23:41 +0530

    Update python_slow_sharp.py

--- piano_code/python_slow_sharp.py
+++ piano_code/python_slow_sharp.py
@@ -2,39 +2,20 @@ import keyboard
 import time
 import serial
     
-Var1=serial.Serial('COM10',9600)
+Var1=serial.Serial('COM12',9600)
 time.sleep(1)
 while 1:
     data=0
     data=Var1.read()
-    if data == 'A':
+    print(data)
+    if data == b'A':
       keyboard.press('z')
-      while data=='A':
+      print ("hell is here friend")
+      while 1:
+        time.sleep(2)
         data = Var1.read()
-        time.sleep(.1)
-      keyboard.realease('z')
-    if data == 'B':
-	    keyboard.press('x')
-	    while data=='B':
-	      data = Var1.read()
-	      time.sleep(.1)
-	    keyboard.realease('x')
-    if data == 'C':
-	    keyboard.press('c')
-	    while data=='B':
-	      data = Var1.read()
-	      time.sleep(.1)
-	    keyboard.realease('c')
-    if data == 'D':
-	    keyboard.press('v')
-	    while data=='B':
-	      data = Var1.read()
-	      time.sleep(.1)
-	    keyboard.realease('v')
-    if data == 'E':
-	    keyboard.press('b')
-	    while data=='B':
-	      data = Var1.read()
-	      time.sleep(.1)
-	    keyboard.realease('b')
-    
+        print ("cello the best company ever")
+        if data != b'A':
+            print ("cello doesnt loook so good now huh")
+            break
+      keyboard.release('z')

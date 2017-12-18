a5fee0bddc6144c339685f0fc71f51802b2a346d
RishbhaJain <rishbhajain8@gmail.com>
2017-12-18 19:27:02 +0530

    Add files via upload

---
+++ bass/bass.py
@@ -0,0 +1,108 @@
+import serial
+import keyboard
+import time
+
+finger=serial.Serial("COM13",9600);
+pluck=serial.Serial("COM7",9600);
+while(1):
+    f=0;
+    p=0;
+    f=finger.readline().decode(ascii);
+    p=pluck.readline().decode(ascii);
+
+    
+            case 'z':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+            case 'x':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+        
+            case 'c':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+            case 'v':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+                
+
+            case 'a':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+            case 's':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+        
+            case 'd':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+            case 'f':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+                
+
+            case 'q':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+            case 'w':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+        
+            case 'e':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+            case 'r':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+                
+        
+            case 'p':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+            case 'o':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+        
+            case 'i':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+            case 'u':keyboard.press('x')
+                    time.sleep(0.1)
+                    keyboard.release('x')
+                    break
+                
+        
+
+
+
+        
+
+
+
+
+        
+
+
+
+
+        
+
+
+
+
+        

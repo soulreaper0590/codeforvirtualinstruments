054c1a301119b096202d28e7ee601d2170c8bab8
Umangi07 <jainumangi17@gmail.com>
2017-12-27 23:18:46 +0530

    Update drums.py

--- BluetoothModule/bluetooth_drums/drums.py
+++ BluetoothModule/bluetooth_drums/drums.py
@@ -6,8 +6,8 @@ from threading import Thread
 
 import win32com.client as comclt#provides a lot of modules so better to keep it
 
-ser1 = serial.Serial('COM4',9600)
-ser2 = serial.Serial('COM16',9600)
+ser1 = serial.Serial('COM6',9600)
+ser2 = serial.Serial('COM9',9600)
 wsh= comclt.Dispatch("WScript.Shell")#used for key press in python.
 
 def firstfunction():
@@ -16,34 +16,34 @@ def firstfunction():
          data1 = ser1.readline().decode('ascii');
         
          if(data1[0]=='F'):   #making an array is an important task in python
-                time.sleep(0.1)
+                time.sleep(0.05)
                 keyboard.write('c')
                 #print(arduinoc);
-                time.sleep(0.1)
+                time.sleep(0.05)
                 keyboard.release('c')
         # data2 = ser2.readline().decode('ascii');
          
         # time.sleep(0.1)
          if(data1[0]=='H'):   #making an array is an important task in python
-                time.sleep(0.1)
-                keyboard.write('s')
+                time.sleep(0.05)
+                keyboard.write('o')
                 #print(arduinoc);
-                time.sleep(0.1)
-                keyboard.release('s')
+                time.sleep(0.05)
+                keyboard.release('o')
                
          if(data1[0]=='B'):   #making an array is an important task in python
-                time.sleep(0.1)
-                keyboard.write('b')
+                time.sleep(0.05)
+                keyboard.write('e')
                 #print(arduinoc);
-                time.sleep(0.1)
-                keyboard.release('b')
+                time.sleep(0.05)
+                keyboard.release('e')
                 
          if(data1[0]=='N'):   #making an array is an important task in python
-                time.sleep(0.1)
-                keyboard.write('v')
+                time.sleep(0.05)
+                keyboard.write('i')
                 #print(arduinoc);
-                time.sleep(0.1)
-                keyboard.release('v')
+                time.sleep(0.05)
+                keyboard.release('i')
                 
 
 def secondfunction():
@@ -52,28 +52,30 @@ def secondfunction():
          data2 = ser2.readline().decode('ascii');
 
          if(data2[0]=='Q'):   #making an array is an important task in python
-                time.sleep(0.1)
-                keyboard.write('z')
+                time.sleep(0.05)
+                keyboard.write('w')
                 #print(arduinoc);
                 time.sleep(0.1)
-                keyboard.release('z')
+                keyboard.release('w')
                 
          if(data2[0]=='W'):   #making an array is an important task in python
-                time.sleep(0.1)
-                keyboard.write('g')
+                time.sleep(0.05)
+                keyboard.write('h')
                 #print(arduinoc);
-                time.sleep(0.1)
-                keyboard.release('g')
+                time.sleep(0.05)
+                keyboard.release('h')
                 
          if(data2[0]=='E'):   #making an array is an important task in python
-                time.sleep(0.1)
-                keyboard.write('d')
+                time.sleep(0.05)
+                keyboard.write('t')
                 #print(arduinoc);
-                time.sleep(0.1)
-                keyboard.release('d')
+                time.sleep(0.05)
+                keyboard.release('t')
 
 t1 = Thread(target = firstfunction)
 t2 = Thread(target = secondfunction)
 t1.start()
 t2.start()
+    
+
     
